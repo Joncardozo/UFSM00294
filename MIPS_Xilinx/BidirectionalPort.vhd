8d43aaa3b721c4dba65a35ab0a927d592e2529e3
Jonathan Cardozo <cardozo@ieee.org>
2025-04-22 14:08:42 -0300

    inicio trabalho 3

---
+++ MIPS_Xilinx/BidirectionalPort.vhd
@@ -0,0 +1,36 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity BidirectionalPort  is
+    generic (
+        DATA_WIDTH          : integer;    -- Port width in bits
+        PORT_DATA_ADDR      : std_logic_vector(1 downto 0);     -- NÃO ALTERAR!
+        PORT_CONFIG_ADDR    : std_logic_vector(1 downto 0);     -- NÃO ALTERAR! 
+        PORT_ENABLE_ADDR    : std_logic_vector(1 downto 0)      -- NÃO ALTERAR!
+    );
+    port (  
+        clk         : in std_logic;
+        rst         : in std_logic; 
+        
+        -- Processor interface
+        data_in     : in std_logic_vector (DATA_WIDTH-1 downto 0);
+        data_out    : out std_logic_vector (DATA_WIDTH-1 downto 0);
+        address     : in std_logic_vector (1 downto 0);     -- NÃO ALTERAR!
+        rw          : in std_logic; -- 0: read; 1: write
+        ce          : in std_logic;
+        
+        -- External interface
+        port_io     : inout std_logic_vector (DATA_WIDTH-1 downto 0)
+    );
+end BidirectionalPort ;
+
+
+architecture Behavioral of BidirectionalPort  is
+begin
+
+	process(clk, rst)
+	begin
+		
+	end process;
+
+end Behavioral;
---
+++ MIPS_Xilinx/mips_uC_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:29:57 04/22/2025 
+// Design Name: 
+// Module Name:    mips_uC_tb 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module mips_uC_tb(
+	input clk, rst
+    );
+
+
+endmodule
